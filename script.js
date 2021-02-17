ce5be28dbe1c12824b54be672493613c2c9cdaee
suarez9093 <suarez9093@hotmail.com>
2021-02-16 20:19:40 -0500

    basic files

---
+++ script.js
@@ -0,0 +1,29 @@
+// Variables
+// ===================================================
+const addUserBtn = document.getElementById('add-user');
+const doubleMoneyBtn = document.getElementById('double');
+const showMillionairesBtn = document.getElementById('show-millionaires');
+const sortBtn = document.getElementById('sort');
+const calculateWealthBtn = document.getElementById('calculate-wealth');
+
+// Functions
+// ===================================================
+function addUser() {}
+
+function doubleMoney() {}
+
+function showOnlyMillionaires() {}
+
+function sortRichest() {}
+
+function calculateTotalWealth() {}
+
+const res = 'https://randomuser.me/api';
+
+// Event Listeners
+// ===================================================
+addUserBtn.addEventListener('click', () => addUser());
+doubleMoneyBtn.addEventListener('click', () => doubleMoney());
+showMillionairesBtn.addEventListener('click', () => showOnlyMillionaires());
+sortBtn.addEventListener('click', () => sortRichest());
+calculateWealthBtn.addEventListener('click', () => calculateTotalWealth());
