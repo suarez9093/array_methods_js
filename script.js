93d97421c931a033416343a9732047d0156470d0
suarez9093 <suarez9093@hotmail.com>
2021-02-20 21:34:32 -0500

    double money working

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ const sortBtn = document.getElementById('sort');
 const calculateWealthBtn = document.getElementById('calculate-wealth');
 let mainContainer = document.getElementById('main');
 
+const userName = document.querySelectorAll('.user-name');
+
 // Functions
 // ===================================================
 async function addUser() {
@@ -21,12 +23,24 @@ async function addUser() {
   };
   //   console.log('user', user);
   let userContainer = document.createElement('div');
-  userContainer.innerHTML = `<div class="user-container"><p>${user.name}</p><p>$${user.money}</p></div>`;
+  userContainer.innerHTML = `<div class="user-container"><p class="user-name">${user.name}</p><p class="user-money">$${user.money}</p></div>`;
   console.log(userContainer);
   return mainContainer.appendChild(userContainer);
 }
 
-function doubleMoney() {}
+function doubleMoney() {
+  const userMoney = document.querySelectorAll('.user-money');
+
+  userMoney.forEach((user) => {
+    let doubleMoney = parseInt(user.innerText.split('$')[1]) * 2;
+    user.innerText = `$${doubleMoney}`;
+    return user;
+  });
+}
+//   for (let i = 0; i < userMoney.length; i++) {
+//     console.log('usermoney', parseInt(userMoney[i].innerText.split('$')[1]));
+//   }
+// }
 
 function showOnlyMillionaires() {}
 
@@ -44,5 +58,5 @@ calculateWealthBtn.addEventListener('click', () => calculateTotalWealth());
 window.addEventListener('DOMContentLoaded', () => {
   addUser();
   addUser();
-  addUser();
+  //   addUser();
 });
