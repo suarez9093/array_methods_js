7b2ee70aedbe236f25de2c58e19cf7e5e05335d3
suarez9093 <suarez9093@hotmail.com>
2021-03-03 11:39:34 -0500

    reduce working

--- script.js
+++ script.js
@@ -5,13 +5,18 @@ const doubleMoneyBtn = document.getElementById('double');
 const showMillionairesBtn = document.getElementById('show-millionaires');
 const sortBtn = document.getElementById('sort');
 const calculateWealthBtn = document.getElementById('calculate-wealth');
-
 const userName = document.querySelectorAll('.user-name');
 let mainContainer = document.getElementById('main');
-
+let users = [];
 // Functions
 // ===================================================
+const formatter = new Intl.NumberFormat('en-US', {
+  style: 'currency',
+  currency: 'USD',
+  minimumFractionDigits: 2,
+});
 async function addUser() {
+  // for each
   const url = 'https://randomuser.me/api';
   let response = await fetch(url);
   let data = await response.json();
@@ -21,39 +26,53 @@ async function addUser() {
     name: `${name.first} ${name.last}`,
     money,
   };
-  //   console.log('user', user);
-  let userContainer = document.createElement('div');
-  userContainer.innerHTML = `<div class="user-container"><p class="user-name">${user.name}</p><p class="user-money">$${user.money}</p></div>`;
-  console.log(userContainer);
-  return mainContainer.appendChild(userContainer);
+  users.push(user);
+  updateDom();
 }
 
-function stringMoneyValueToInt(string) {
-  return parseInt(string.innerText.split('$')[1]);
-}
-
-function doubleMoney() {
-  const userMoney = document.querySelectorAll('.user-money');
-  userMoney.forEach((user) => {
-    let doubleMoney = stringMoneyValueToInt(user) * 2;
-    user.innerText = `$${doubleMoney}`;
-    return user;
+function updateDom(update = users) {
+  // Loop over the users array and update the dom with the change
+  mainContainer.innerHTML = '<h2><strong>Person</strong> Wealth</h2>';
+  update.forEach((user) => {
+    const p = document.createElement('p');
+    p.classList.add('user');
+    p.innerHTML = `<strong>${user.name}</strong> ${formatter.format(
+      user.money
+    )}`;
+    mainContainer.append(p);
   });
+}
+function doubleMoney() {
+  // map over the users array and return a new array with the wealth doubled (map)
+  users = users.map((user) => {
+    return { ...user, money: user.money * 2 };
+  });
+  updateDom();
 }
 
 function showOnlyMillionaires(e) {
-  const userMoney = document.querySelectorAll('.user-money');
-  let userMoneyArray = [...userMoney];
-  userMoneyArray.map((user) => {
-    if (!(stringMoneyValueToInt(user) >= 1000000)) {
-      user.parentElement.style.display = 'none';
-    }
-  });
+  users = users.filter((user) => user.money >= 1000000);
+  updateDom();
 }
 
-function sortRichest() {}
+function sortRichest() {
+  // sort method
+  console.log(users);
+  users.sort((a, b) => b.money - a.money);
+  updateDom();
+  console.log(users);
+}
 
-function calculateTotalWealth() {}
+function calculateTotalWealth() {
+  console.log(users);
+  const total = users.reduce((acc, user) => {
+    return acc + user.money;
+  }, 0);
+  mainContainer.innerHTML = '<h2><strong>Total</strong> Wealth</h2>';
+  const p = document.createElement('p');
+  p.innerHTML = `<p>${formatter.format(total)}</p>`;
+  mainContainer.append(p);
+}
 
 // Event Listeners
 // ===================================================
@@ -64,6 +83,4 @@ sortBtn.addEventListener('click', () => sortRichest());
 calculateWealthBtn.addEventListener('click', () => calculateTotalWealth());
 window.addEventListener('DOMContentLoaded', () => {
   addUser();
-  addUser();
-  //   addUser();
 });
